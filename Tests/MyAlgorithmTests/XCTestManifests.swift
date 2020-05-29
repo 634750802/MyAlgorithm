5ee58705b875a288dbde25ba5e3d2dbfdee63b36
Jagger <634750802@qq.com>
2020-05-29 19:08:27 +0800

    update test

--- Tests/MyAlgorithmTests/XCTestManifests.swift
+++ Tests/MyAlgorithmTests/XCTestManifests.swift
@@ -2,9 +2,10 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
+    [
         testCase(QueueTest.allTests),
-        testCase(LinkTest.allTests)
+        testCase(LinkTest.allTests),
+        testCase(HeapTest.allTests)
     ]
 }
 #endif
